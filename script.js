a2b4fd54bb8558d81124ba3a3499f134fa8e5930
CRISTIAN <104232321+cristiangarcia03@users.noreply.github.com>
2023-10-25 22:10:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
+var nav = document.getElementById("navlist");
+var header = document.getElementById("header");
 
+var button = document.getElementById("menu");
 
-document.addEventListener('click', e => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]")
-  if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
-
-  let currentDropdown
-  if (isDropdownButton) {
-      currentDropdown = e.target.closest('[data-dropdown]')
-      currentDropdown.classList.toggle('active')
-  }
-
-  document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
-    if (dropdown === currentDropdown) return
-    dropdown.classList.remove('active')
-  })
-})
-
-var btc = document.getElementById("bitcoin")
-var eth = document.getElementById("ethereum")
-var dog = document.getElementById("dogecoin")
-
-var liveprice = {
-  "async": true,
-  "scroosDomain": true,
-  "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
-
-  "method" : "GET",
-  "headers": {}
+function fun(){
+    nav.classList.toggle('open');
+    if (header.style.overflow == 'visible') {
+        header.style.overflow = 'hidden'
+    } else {
+        header.style.overflow = 'visible'
+    }
 }
 
-$.ajax(liveprice).done(function (response){
-    btc.innerHTML = response.bitcoin.usd
-    eth.innerHTML = response.ethereum.usd
-    dog.innerHTML = response.dogecoin.usd
-});
+button.addEventListener("click", fun)
+
+
+var python_page = document.getElementById("python_page")
+var html_page = document.getElementById("html_page")
+var swift_page = document.getElementById("swift_page")
+
+var python_btn = document.getElementById("python_btn")
+var html_btn = document.getElementById("html_btn")
+var swift_btn = document.getElementById("swift_btn")
+
+var proj = document.getElementById("projects")
+
+function openPython() {
+    python_page.style.transform = "translateX(0)"
+    html_page.style.transform = "translateX(120%)"
+    swift_page.style.transform = "translateX(120%)"
+    python_btn.style.color = "#ff7846"
+    html_btn.style.color = "#fff"
+    swift_btn.style.color = "#fff"
+    proj.style.height = "900px"
+}
+
+function openHtml() {
+    python_page.style.transform = "translateX(120%)"
+    html_page.style.transform = "translateX(0)"
+    swift_page.style.transform = "translateX(120%)"
+    python_btn.style.color = "#fff"
+    html_btn.style.color = "#ff7846"
+    swift_btn.style.color = "#fff"
+    proj.style.height = "500px"
+}
+
+function openSwift() {
+    python_page.style.transform = "translateX(120%)"
+    html_page.style.transform = "translateX(120%)"
+    swift_page.style.transform = "translateX(0)"
+    python_btn.style.color = "#fff"
+    html_btn.style.color = "#fff"
+    swift_btn.style.color = "#ff7846"
+    proj.style.height = "500px"
+}
+
+
+function sendEmail(){
+    (function(){
+      emailjs.init("IIfEX21RR-pQQuRnF"); // Account Public Key
+    })();
+
+    var params = {
+      sendername: document.getElementById("input_name").value,
+      email: document.getElementById("input_email").value,
+      message: document.getElementById("email_message").value,
+    };
+
+    var serviceID = "service_xy2045v"; // Email Service ID
+    var templateID = "template_jzsphvg"; // Email Template ID
+
+    emailjs.send(serviceID, templateID, params)
+    .then( res => {
+        alert("Email sent successfully!!")
+    })
+    .catch();
+  }
